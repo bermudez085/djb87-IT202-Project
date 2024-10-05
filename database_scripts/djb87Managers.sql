5d980d7eaa8bab14cc6bc42d996cca619fcb961c
Daniel <danieljesusbermudez@gmail.com>
2024-10-04 20:05:23 -0400

    recreating project

---
+++ database_scripts/djb87Managers.sql
@@ -0,0 +1,26 @@
+CREATE TABLE GourmetFoodsManagers (
+    GourmetManagerID     INT(11)        NOT NULL   
+    AUTO_INCREMENT,
+    emailAddress         VARCHAR(255)   NOT NULL   
+    UNIQUE,
+    password             VARCHAR(64)    NOT NULL,
+    pronouns             VARCHAR(60)    NOT NULL,
+    firstName            VARCHAR(60)    NOT NULL,
+    lastName             VARCHAR(60)    NOT NULL,
+    dateCreated          DATETIME       NOT NULL,
+    PRIMARY KEY (GourmetManagerID)
+);
+INSERT INTO GourmetFoodsManagers
+(emailAddress, password, pronouns, firstName, lastName, dateCreated)
+VALUES
+('christian@gmail.com', SHA2('christians', 256), 'He/Him', 'Christian', 'Bermudez', NOW());
+
+INSERT INTO GourmetFoodsManagers
+(emailAddress, password, pronouns, firstName, lastName, dateCreated)
+VALUES
+('jose@gmail.com', SHA2('joses', 256), 'He/Him', 'Jose', 'Jones', NOW());
+
+INSERT INTO GourmetFoodsManagers
+(emailAddress, password, pronouns, firstName, lastName, dateCreated)
+VALUES
+('daniel@gmail.com', SHA2('daniels', 256), 'He/Him', 'Daniel', 'Smith', NOW());
